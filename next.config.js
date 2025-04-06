a7232e6fc5c075ab48002eaf9d0decc4f94df97f
vatistasdimitris01 <vatistasdimitris.01@gmail.com>
2025-04-06 15:53:45 +0300

    Simplify Next.js and Vercel configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false,
-  env: {
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -19,11 +15,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Improve build performance
-  poweredByHeader: false,
-  // For Vercel deployment
-  reactStrictMode: false,
-  trailingSlash: true,
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  }
 };
 
 module.exports = nextConfig; 
